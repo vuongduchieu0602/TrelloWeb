5d4a897100f613fe1bc3ef159a8e1390f5feabfe
Vuong Duc Hieu <vg.duchieu0602@gmail.com>
2023-12-18 22:32:01 +0700

    dark_light_part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
