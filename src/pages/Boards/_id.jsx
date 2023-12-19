13f384eae826eb35bef15d7668c870bcd1fc13bd
Vuong Duc Hieu <vg.duchieu0602@gmail.com>
2023-12-20 00:06:22 +0700

    Refactor Project Structure

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,20 @@
+//Board detail
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+
+import AppBar from '../../components/AppBar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+
+
+function Board() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  )
+}
+
+export default Board
